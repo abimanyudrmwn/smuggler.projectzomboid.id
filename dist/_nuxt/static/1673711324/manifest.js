f89079ccd0db6c1203669ffce5f363fde23427d2
abimanyudrmwn <abimanyudarmawan4@gmail.com>
2023-01-14 22:54:15 +0700

    Editing some Content v3

---
+++ dist/_nuxt/static/1673711324/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fwiki","\u002Fmods","\u002Fcontributors","\u002Fwiki\u002Fwfe-items-list"]})
---
+++ dist/_nuxt/static/1673711324/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673711324",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"93577d4a"}}}}(null,"\u002F"));
