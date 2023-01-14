da7e6097ba801001f944873c85bc8b97c092993d
abimanyudrmwn <abimanyudarmawan4@gmail.com>
2023-01-14 22:43:16 +0700

    Added some content

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -4,7 +4,7 @@
         <div class="mx-auto max-w-5xl px-6 xl:px-0 py-32">
             <h2 class="text-2xl font-bold font-sora mb-6 text-white">About</h2>
         <div>
-            <p class="font-sora tracking-widest font-medium text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugiat totam facilis optio, corporis qui commodi! Molestiae qui excepturi dicta repellat, inventore voluptatem praesentium impedit! Nemo animi eligendi architecto tempora incidunt, repellat cupiditate nulla quam magnam nobis deserunt eveniet velit odit magni ipsum vitae, laborum, aperiam officia cumque assumenda. Maiores, itaque. Mollitia voluptatum amet doloremque aliquid dolorem quibusdam, rerum voluptatem veritatis. Quod, voluptates similique.</p>
+            <p class="font-sora tracking-widest font-medium text-gray-300">This website is the go-to destination for all mods created by the Indonesia Project Zomboid community. Here you will find a wide variety of mods that have been crafted by the talented members.</p>
         </div>
         </div>
     </div>
