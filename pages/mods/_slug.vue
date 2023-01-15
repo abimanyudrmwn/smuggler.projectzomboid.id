1d995b6ab70ce847ba0c65bb33d0dde40d705331
abimanyudrmwn <abimanyudarmawan4@gmail.com>
2023-01-15 17:13:30 +0700

    Editing Metatags for SEO

--- pages/mods/_slug.vue
+++ pages/mods/_slug.vue
@@ -40,68 +40,44 @@
         .fetch();
       return { mods, prev, next };
     },
-    components: { YoutubeVideo },
-    head () {
-    return {
-      title: `${this.article.title} - DrmwnSpace`,
-      meta: [
-        { 
-          name: 'author', 
-          content: 'Abimanyu Darmawan' 
-        },
-        {
-          name: 'description',
-          content: this.article.description
-        },
-        {
-          key: 'og:title',
-          property: 'og:title',
-          content: `${this.article.title}`,
-        },
-        {
-          key: 'og:image',
-          property: 'og:image',
-          content: `https://drmwn.space/images/covers/${article.coverImage}`,
-        },
-        {
-          key: 'og:description',
-          property: 'og:description',
-          content: this.article.description,
-        },
-        {
-          key: 'og:url',
-          property: 'og:url',
-          content: `https://drmwn.space${this.article.path}`,
-        },
-        {
-          key: 'twitter:title',
-          name: 'twitter:title',
-          content: `${this.article.title}`,
-        },
-        
-        {
-          key: 'twitter:description',
-          name: 'twitter:description',
-          content: this.article.frase,
-        },
-        {
-          key: 'twitter:creator',
-          name: 'twitter:creator',
-          content: '@abimanyudrmwn',
-        },
-        {
-          key: 'twitter:card',
-          name: 'twitter:card',
-          content: 'frase',
-        },
-        {
-          key: 'twitter:image',
-          name: 'twitter:image',
-          content: `https://drmwn.space${this.article.image}`,
-        }
-      ]
-    }
+    async fetch() {
+    const { data } = await this.$axios.get(
+      `api_url`
+    );
+    this.mods = data.response.results[0];
+    this.loading = false;
   },
+  data() {
+    return {
+      mods: {},
+    };
+  },
+  head() {
+    return {
+      title: this.mods.title ? `${this.mods.title}` : "",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.mods.frase,
+        },
+        { hid: "og-title", property: "og:title", content: this.mods.title },
+        {
+          hid: "og-image",
+          property: "og:image",
+          content: this.mods.coverImage,
+        },
+        { hid: "og-image-width", property: "og:image:width", content: 500 },
+        { hid: "og-image-height", property: "og:image:height", content: 300 },
+        {
+          hid: "og-image-type",
+          property: "og:image:type",
+          content: "image/jpeg",
+        },
+      ],
+    };
+  },
+    components: { YoutubeVideo },
   };
   </script>
   
